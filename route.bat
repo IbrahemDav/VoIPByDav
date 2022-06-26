0da8ed5f3885bdf72ca005e443a1b48d8d288734
Ibrahem Mohamed <107731647+IbrahemDav@users.noreply.github.com>
2022-06-26 23:42:12 +0200

    Update route.bat

--- route.bat
+++ route.bat
@@ -1,6 +1,6 @@
 @echo off
 set ip="IP Address"
-for /f "tokens=3 delims=: " %%I in ('netsh interface IPv4 show addresses "MajorAmariVPN" ^| findstr /C:%ip%') do set ip_address=%%I
+for /f "tokens=3 delims=: " %%I in ('netsh interface IPv4 show addresses "DavVPN" ^| findstr /C:%ip%') do set ip_address=%%I
 route add 3.0.0.0 mask 255.0.0.0 %ip_address%
 route add 8.0.0.0 mask 255.0.0.0 %ip_address%
 route add 18.0.0.0 mask 255.0.0.0 %ip_address%
